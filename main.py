82080ec225ac09e503053bc4bb9e6c9f0c375d82
Muhammad Umair <muhammadumair894@gmail.com>
2023-10-17 18:57:27 +0500

    Update main.py

--- main.py
+++ main.py
@@ -10,9 +10,14 @@ from langchain.vectorstores import FAISS
 from langchain.chains import ConversationalRetrievalChain
 import logging
 from fastapi import FastAPI, File, UploadFile
+from typing import List
 import requests
+import nest_asyncio
+from pyngrok import ngrok
+import uvicorn
 
-logging.disable(logging.WARNING)
+
+#logging.disable(logging.WARNING)
 
 model_id = 'meta-llama/Llama-2-7b-chat-hf'
 
@@ -38,7 +43,7 @@ model = transformers.AutoModelForCausalLM.from_pretrained(
     model_id,
     trust_remote_code=True,
     config=model_config,
-    quantization_config=bnb_config,
+    #quantization_config=bnb_config,
     device_map='auto',
     use_auth_token=hf_auth
 )
@@ -87,3 +92,38 @@ generate_text = transformers.pipeline(
 )
 
 llm = HuggingFacePipeline(pipeline=generate_text)
+
+model_name = "sentence-transformers/all-mpnet-base-v2"
+model_kwargs = {"device": "cuda"} #cpu cuda
+
+embeddings = HuggingFaceEmbeddings(model_name=model_name, model_kwargs=model_kwargs)
+
+description = """
+## Medical Document QA
+This app shows how to do Document Question Answering
+Check out the docs for the `/predict` endpoint below to try it out!
+"""
+app = FastAPI(docs_url="/", description=description)
+@app.post("/predict")
+async def load_file(file_url: str, sentences: List[str]):
+
+    loader = PyPDFLoader(file_url)
+    pages = loader.load_and_split()
+    #pages[0]
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=4000, chunk_overlap=20)
+    all_splits = text_splitter.split_documents(pages)
+    # storing embeddings in the vector store
+    vectorstore = FAISS.from_documents(all_splits, embeddings)
+
+    chain = ConversationalRetrievalChain.from_llm(llm, vectorstore.as_retriever(), return_source_documents=True)
+
+    chat_history = []
+    qa_dict = {}
+    for question in sentences:
+      result = chain({"question": question, "chat_history": chat_history})
+      print(f"Question: {question}")
+      print(f"Answer: {(result['answer']).strip()}")
+      qa_dict[question] = (result['answer']).strip()
+
+    return qa_dict
+
